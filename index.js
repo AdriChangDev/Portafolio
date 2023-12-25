32a3cb9a3c6b2fdbb0ab175be88876526165e18e
Adrian Chang <121107149+AdriChangDev@users.noreply.github.com>
2023-12-26 00:20:07 +0100

    changed css , html and js

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-$(document).ready(function() {
-    const filterContainer = $(".gallery__categories");
-    const galleryItems = $(".project__card");
+document.addEventListener("DOMContentLoaded", function() {
 
-    filterContainer.on("click", ".filter-item", function() {
-      const $target = $(this);
-      if (!$target.hasClass("active")) {
-        filterContainer.find(".active").removeClass("active");
-        $target.addClass("active");
-        const filterValue = $target.data("filter");
-        galleryItems.each(function() {
-          const $item = $(this);
-          if ($item.hasClass(filterValue) || filterValue === "all") {
-            $item.removeClass("hide").addClass("show");
-          } else {
-            $item.removeClass("show").addClass("hide");
-          }
-        });
-      }
+    const filterContainer = document.querySelector(".gallery__categories");
+    const galleryItems = document.querySelectorAll(".project__card");
+
+    filterContainer.addEventListener("click", function(event) {
+        const target = event.target;
+        if (target.classList.contains("filter-item") && !target.classList.contains("active")) {
+            const activeFilter = filterContainer.querySelector(".active");
+            if (activeFilter) {
+                activeFilter.classList.remove("active");
+            }
+            target.classList.add("active");
+
+            const filterValue = target.getAttribute("data-filter");
+            galleryItems.forEach(function(item) {
+                if (item.classList.contains(filterValue) || filterValue === "all") {
+                    item.classList.remove("hide");
+                    item.classList.add("show");
+                } else {
+                    item.classList.remove("show");
+                    item.classList.add("hide");
+                }
+            });
+        }
     });
 
-    function scrollToSection(sectionId) {
-      $("#" + sectionId)[0].scrollIntoView({ behavior: "smooth" });
-    }
+    
 });
+function scrollToSection(sectionId) {
+        document.getElementById(sectionId).scrollIntoView({ behavior: "smooth" });
+    }
